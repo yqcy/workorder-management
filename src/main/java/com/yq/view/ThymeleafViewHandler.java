9e222de2178d7c67e748562ab115cca65e4c489c
yangqing <neteasy_yqcy@163.com>
2017-11-12 12:27:52 +0800

    修改index.html，加入Model

--- src/main/java/com/yq/view/ThymeleafViewHandler.java
+++ src/main/java/com/yq/view/ThymeleafViewHandler.java
@@ -1,7 +1,13 @@
 package com.yq.view;
 
+import com.yq.entity.Workorder;
+import com.yq.service.WorkorderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * created by wb-yq264139 on 2017/11/10
@@ -9,8 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ThymeleafViewHandler {
 
+    @Autowired
+    private WorkorderService workorderService;
+
     @RequestMapping(value = "/index.html")
-    public String index() {
+    public String index(Model model) {
+        List<Workorder> list = this.workorderService.query(null, null);
+        model.addAttribute("workorders", list);
         return "index";
     }
 }
